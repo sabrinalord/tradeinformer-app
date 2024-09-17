a9bdecbbe6bdc1c676374071eec75980b4bb8698
Sabrina Lord <sabrinalord@Sabrinas-Air.home>
2024-09-17 16:36:14 +0100

    added apollo client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import ApolloWrapper from "../lib/ApolloWrapper";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ApolloWrapper>
         {children}
+        </ApolloWrapper>
+      
       </body>
     </html>
   );
