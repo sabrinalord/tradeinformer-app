2054ca702d4d5f1297132e2c62da5b47303f38f9
Sabrina Lord <sabrinalord@Sabrinas-Air.QLOCOM>
2024-11-06 20:59:36 +0000

    update env

--- src/app/components/Advert.tsx
+++ src/app/components/Advert.tsx
@@ -41,6 +41,9 @@ export default function Advert(props: AdvertProps) {
                     method: 'GET',
                     headers: {
                       'Content-Type': 'application/json', 
+                      'Access-Control-Allow-Origin': '*', 
+                      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                      'Access-Control-Allow-Headers': 'Content-Type',
                     },
                   });
 
