4f54eff6d89c1a00b4022265409a8ad556bdf775
Sabrina Lord <sabrinalord@sabrinas-air.home>
2024-11-28 10:14:57 +0000

    update params to comply with next 15 update

--- src/app/[category]/[slug]/page.tsx
+++ src/app/[category]/[slug]/page.tsx
@@ -31,7 +31,11 @@ export async function generateStaticParams() {
 
 }
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default async function Page({
+    params,
+  }: {
+    params: Promise<{ slug: string }>;
+  }) {
     const { slug } = await params
 
     const headerMenuData: MenuResponse = await fetchHeaderMenu();
