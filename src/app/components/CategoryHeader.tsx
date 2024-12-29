383132dd08d20df84c8defa5ce8ccf47aba04085
Sabrina Lord <sabrinalord@sabrinas-air.home>
2024-12-29 14:50:44 +0000

    remove big gap

--- src/app/components/CategoryHeader.tsx
+++ src/app/components/CategoryHeader.tsx
@@ -6,7 +6,7 @@ interface CategoryHeaderProps {
 
 const CategoryHeader: React.FC<CategoryHeaderProps> = ({ categoryName }) => {
   return (
-    <span className="flex items-center mb-4">
+    <span className="flex items-center">
       <h1 className="ml-2 font-bold uppercase text-lg">{categoryName}</h1>
       <div className="flex-1 border-t border-gray-300 ml-2"></div>
     </span>
