057a7e1ebcff15b58d11a26613bce3f9f0256d72
Sabrina Lord <sabrinalord@sabrinas-air.home>
2025-01-01 13:25:04 +0000

    update header for local dev and add security he aders

--- src/app/api/widget/route.ts
+++ src/app/api/widget/route.ts
@@ -13,9 +13,19 @@ export async function GET() {
 
         const headers = {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'www.tradeinformer.com',
+            'Access-Control-Allow-Origin': process.env.NODE_ENV === 'development' 
+            ? 'http://localhost:3000' 
+             : 'https://www.tradeinformer.com',  
             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type',
+            'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',  // Enforces HTTPS connections
+            'X-Content-Type-Options': 'nosniff',  // Prevents MIME type sniffing, which helps to prevent XSS
+            'X-Frame-Options': 'DENY',  // Prevents page to be embedded in an iframe
+            'X-XSS-Protection': '1; mode=block',  
+            'Referrer-Policy': 'strict-origin-when-cross-origin',  // Ensures that referrer headers are sent securely
+            'Cache-Control': 'no-store, max-age=0, must-revalidate', // Prevent caching for sensitive API data 
+            'Pragma': 'no-cache',  // Prevents caching for older HTTP/1.0 caches
+            'Expires': '0',  // Ensures that caches do not store sensitive data
         };
 
         return NextResponse.json(banners, { headers });
